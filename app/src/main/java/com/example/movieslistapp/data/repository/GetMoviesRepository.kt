8bf7acf5198d4ba42e9e5ef4665c26aadaebdda0
Qkprahlad101 <prahladkumar@Mac-239.lan>
2026-02-19 14:27:50 +0530

    LRU implemented.

--- app/src/main/java/com/example/movieslistapp/data/repository/GetMoviesRepository.kt
+++ app/src/main/java/com/example/movieslistapp/data/repository/GetMoviesRepository.kt
@@ -1,6 +1,8 @@
 package com.example.movieslistapp.data.repository
 
+import androidx.collection.LruCache
 import com.example.movieslistapp.data.ApiService
+import com.example.movieslistapp.data.model.Movie
 import com.example.movieslistapp.data.model.MovieDetails
 import com.example.movieslistapp.data.model.MovieResponse
 import com.example.movieslistapp.db.dao.MovieDao
@@ -13,16 +15,27 @@ class GetMoviesRepository(
     private val apiService: ApiService,
     private val movieDao: MovieDao
 ) {
-    suspend fun getMoviesListFromSearch(query: String, currentPage: Int): MovieResponse {
-        val cachedMovies = movieDao.getMoviesByQuery(query)
 
-        return if (cachedMovies.isNotEmpty()) {
+    private val movieDetailsCache = LruCache<String, MovieDetails>(50)
+    private val moviesListCache = LruCache<String, MovieResponse>(20)
+    suspend fun getMoviesListFromSearch(query: String, currentPage: Int): MovieResponse {
+
+        //check in cache
+        moviesListCache.get(query)?.let {
+            return it
+        }
+
+        //check in db
+        val dbMoviesList = movieDao.getMoviesByQuery(query)
+        return if (dbMoviesList.isNotEmpty()) {
             MovieResponse(
-                Search = cachedMovies.map { it.toMovie() },
+                Search = dbMoviesList.map { it.toMovie() },
                 Error = "",
                 Response = "True"
             )
         } else {
+
+            //api call
             val response = apiService.getMoviesListFromSearch(query, currentPage)
             response.Search?.let { movies ->
                 movieDao.insertMovies(movies.map { it.toMovieEntity(query) })
@@ -32,11 +45,17 @@ class GetMoviesRepository(
     }
 
     suspend fun getMovieDetails(imdbId: String): MovieDetails {
-        val cachedDetails = movieDao.getMovieDetails(imdbId)
+        //first check in cache and return it
+        movieDetailsCache.get(imdbId)?.let {
+            return it
+        }
 
-        return if (cachedDetails != null) {
-            cachedDetails.toMovieDetails()
+        //check database
+        val dbDetails = movieDao.getMovieDetails(imdbId)
+        return if (dbDetails != null) {
+            dbDetails.toMovieDetails()
         } else {
+            //api call
             val details = apiService.getMovieDetails(imdbId)
             movieDao.insertMovieDetails(details.toMovieDetailsEntity())
             details
